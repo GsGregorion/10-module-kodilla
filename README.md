4d9e2ce4d03d644c10c4eab77b7fe7b6765d6f4d
Gregorion Skquarna <gs.gregorion@gmail.com>
2018-09-13 20:55:52 +0200

    Finish task 10.1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 10.1-plugin
+
+https://gsgregorion.github.io/10.1-plugin/
+
+https://codepen.io/gregorion/pen/xarBLa
--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 'use strict'
 var elem = document.querySelector('.main-carousel');
+
 var flkty = new Flickity( elem, {
   // options
   pageDots: false,
@@ -10,140 +11,29 @@ var flkty = new Flickity( elem, {
   contain: true
 });
 
-var flkty = new Flickity('.main-carousel');
-
 var progressBar = document.querySelector('.progress-bar')
 
 flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
-var firstSlide = document.getElementbyId('carousel-cell-1');
-var showFirst = function(){
-  scrollTo.firstSlide;
-}
 
-var goBackButton = document.getElementById('goBack');
-
-goBackButton.addEventListener('click', showFirst);
-/*!
- * Flickity hash v1.0.3
- * Enable hash navigation for Flickity
- */
-
-/*jshint browser: true, undef: true, unused: true, strict: true*/
-
-( function( window, factory ) {
-  // universal module definition
-  /*jshint strict: false */ /*globals define, module, require */
-  if ( typeof define == 'function' && define.amd ) {
-    // AMD
-    define( [
-      'flickity/js/index',
-    ], factory );
-  } else if ( typeof module == 'object' && module.exports ) {
-    // CommonJS
-    module.exports = factory(
-      require('flickity')
-    );
-  } else {
-    // browser global
-    factory(
-      window.Flickity
-    );
-  }
-
-}( window, function factory( Flickity ) {
-
-'use strict';
-
-Flickity.createMethods.push('_createHash');
-
-var proto = Flickity.prototype;
-
-proto._createHash = function() {
-  if ( !this.options.hash ) {
-    return;
-  }
-  this.connectedHashLinks = [];
-  // hash link listener
-  // use HTML5 history pushState to prevent page scroll jump
-  this.onHashLinkClick = function( event ) {
-    event.preventDefault();
-    this.selectCell( event.currentTarget.hash );
-    history.replaceState( null, '', event.currentTarget.hash );
-  }.bind( this );
-
-  // events
-  this.on( 'activate', this.activateHash );
-  this.on( 'deactivate', this.deactivateHash );
-};
-
-proto.activateHash = function() {
-  this.on( 'change', this.onChangeHash );
-
-  // overwrite initialIndex
-  if ( this.options.initialIndex === undefined && location.hash ) {
-    var cell = this.queryCell( location.hash );
-    if ( cell ) {
-      this.options.initialIndex = this.getCellSlideIndex( cell );
-    }
-  }
-
-  this.connectHashLinks();
-};
+var reStartButton = document.getElementById('goBack');
+var goBackButton = document.getElementById('prevBtn');
+var goForwardButton = document.getElementById('nextBtn');
 
 
-proto.deactivateHash = function() {
-  this.off( 'change', this.onChangeHash );
-  this.disconnectHashLinks();
-};
+reStartButton.addEventListener( 'click', function( event ) {
+  // filter for button clicks
+  flkty.select( 0 );
+});
 
-proto.onChangeHash = function() {
-  var id = this.selectedElement.id;
-  if ( id ) {
-    var url = '#' + id;
-    history.replaceState( null, '', url );
-  }
-};
+goBackButton.addEventListener( 'click', function( event ) {
+  // filter for button clicks
+  flkty.previous(  );
+});
 
-
-proto.connectHashLinks = function() {
-  var links = document.querySelectorAll('a');
-  for ( var i=0; i < links.length; i++ ) {
-    this.connectHashLink( links[i] );
-  }
-};
-
-// used to test if link is on same page
-var proxyLink = document.createElement('a');
-
-proto.connectHashLink = function( link ) {
-  if ( !link.hash ) {
-    return;
-  }
-  // check that link is for the same page
-  proxyLink.href = link.href;
-  if ( proxyLink.pathname != location.pathname ) {
-    return;
-  }
-  var cell = this.queryCell( link.hash );
-  if ( !cell ) {
-    return;
-  }
-  link.addEventListener( 'click', this.onHashLinkClick );
-  this.connectedHashLinks.push( link );
-};
-
-proto.disconnectHashLinks = function() {
-  this.connectedHashLinks.forEach( function( link ) {
-    link.removeEventListener( 'click', this.onHashLinkClick );
-  }, this );
-  this.connectedHashLinks = [];
-};
-
-// -----  ----- //
-
-return Flickity;
-
-}));
+goForwardButton.addEventListener( 'click', function( event ) {
+  // filter for button clicks
+  flkty.next(  );
+});
