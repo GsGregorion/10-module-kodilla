6bc86734d5f4fce683c1e7c950051dbb0e5b2aa0
Gregorion Skquarna <gs.gregorion@gmail.com>
2018-09-15 16:54:04 +0200

    Finish task 10.2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
 'use strict'
-var elem = document.querySelector('.main-carousel');
+var templateSlide = document.getElementById('template-slide-mustache').innerHTML,
+  templateLinks = document.getElementById('template-my-links').innerHTML,
+  slideItems = '',
+  slideLinks = '';
 
+	Mustache.parse(templateSlide);
+  Mustache.parse(templateLinks);	
+
+		
+for ( var i = 0; i < slidesInnerData.length; i++) {
+    console.log(slidesInnerData);
+    
+		slideItems += Mustache.render(templateSlide, slidesInnerData[i]);
+    slideLinks += Mustache.render(templateLinks, slidesInnerData[i]);
+}
+	
+  generatedMySlides.insertAdjacentHTML('beforeend', slideItems);
+  generatedMyLinks.insertAdjacentHTML('beforeend', slideLinks);
+  
+
+var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
   pageDots: false,
