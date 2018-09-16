621bbb842cd0a92f2865bdccc3fd5c20070a03ac
Gregorion Skquarna <gs.gregorion@gmail.com>
2018-09-16 10:38:48 +0200

    Init basic google map according to the documentation

--- js/index.js
+++ js/index.js
@@ -56,3 +56,14 @@ goForwardButton.addEventListener( 'click', function( event ) {
   // filter for button clicks
   flkty.next(  );
 });
+
+
+function initMap() {
+  var uluru = {lat: -25.344, lng: 131.036};
+  
+  var map = new google.maps.Map(
+    document.getElementById('map'), {zoom: 4, center: uluru});
+  
+  var marker = new google.maps.Marker({position: uluru, map: map})
+  
+}
